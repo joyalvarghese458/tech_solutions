51b0df1dc2d27a303a890dbf782b4abb6808304d
Joyal Varghese <joyalvarghese458@gmail.com>
2026-01-13 15:51:59 +0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
         <Route path='/portfolio' element={<Portfolio />} />
         <Route path='/careers' element={<Careers />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<Home />} />
       </Routes>
       <Footer />
     </Router>
